5bdb4e22c36255c7dbd31fbb896474da04391de5
wenked <wenked.matheus@yahoo.com.br>
2022-03-22 17:53:02 -0300

    criando db

--- main.py
+++ main.py
@@ -2,6 +2,18 @@ from selenium import webdriver
 from webdriver_manager.firefox import GeckoDriverManager
 from selenium.common import exceptions
 import time
+import sqlite3
+
+db = sqlite3.connect('augments.db')
+db.set_trace_callback(print)
+cursor = db.cursor()
+
+cursor.execute('CREATE TABLE IF NOT EXISTS augments (id INTEGER PRIMARY KEY, name TEXT,tier TEXT, pickrate TEXT, placement TEXT, top4 TEXT, winrate TEXT, stage14 TEXT, stage33 TEXT, stage46 TEXT)')
+db.commit()
+
+def insert_data(data):
+      cursor.execute('INSERT INTO augments (name, tier, pickrate, placement, top4, winrate, stage14, stage33, stage46) VALUES (?,?,?,?,?,?,?,?,?)', data)
+      db.commit()
 
 driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
 
@@ -46,31 +58,31 @@ def adding_filters(tier='silver'):
 ## place --   /html/body/div[1]/div/div/div/div[2]/div/div[4]/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div[2]/div[1]/div[2]
 ## top4 --    /html/body/div[1]/div/div/div/div[2]/div/div[4]/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div[2]/div[1]/div[3]
 
-def get_data():
+def get_data(tier):
   counter_linha_nome = 2
   counter_linha = 1
-  data = []
+
   nome =  driver.find_element_by_xpath(f'/html/body/div[1]/div/div/div/div[2]/div/div[4]/div[2]/div/div[2]/div/div/div[2]/div/div[1]/div[{counter_linha_nome}]/div/div/div[2]')
   while nome:
       augment = {}
       try:
-        nome =  driver.find_element_by_xpath(f'/html/body/div[1]/div/div/div/div[2]/div/div[4]/div[2]/div/div[2]/div/div/div[2]/div/div[1]/div[{counter_linha_nome}]/div/div/div[2]')
-        augment['nome'] = nome.text
+        nome =  driver.find_element_by_xpath(f'/html/body/div[1]/div/div/div/div[2]/div/div[4]/div[2]/div/div[2]/div/div/div[2]/div/div[1]/div[{counter_linha_nome}]/div/div/div[2]').text
       except exceptions.NoSuchElementException:
         nome = False
         continue     
-      pickrate = driver.find_element_by_xpath(f'/html/body/div[1]/div/div/div/div[2]/div/div[4]/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div[2]/div[{counter_linha}]/div[1]/div')
-      place = driver.find_element_by_xpath(f'/html/body/div[1]/div/div/div/div[2]/div/div[4]/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div[2]/div[{counter_linha}]/div[2]')
-      top4 = pickrate = driver.find_element_by_xpath(f'/html/body/div[1]/div/div/div/div[2]/div/div[4]/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div[2]/div[{counter_linha}]/div[3]')
-      augment['pickrate'] = pickrate.text
-      augment['place'] = place.text
-      augment['top4'] = top4.text
-      print(augment)
-      data.append(augment)
+      pickrate = driver.find_element_by_xpath(f'/html/body/div[1]/div/div/div/div[2]/div/div[4]/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div[2]/div[{counter_linha}]/div[1]/div').text
+      place = driver.find_element_by_xpath(f'/html/body/div[1]/div/div/div/div[2]/div/div[4]/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div[2]/div[{counter_linha}]/div[2]').text
+      top4 = driver.find_element_by_xpath(f'/html/body/div[1]/div/div/div/div[2]/div/div[4]/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div[2]/div[{counter_linha}]/div[3]').text
+      win = driver.find_element_by_xpath(f'/html/body/div[1]/div/div/div/div[2]/div/div[4]/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div[2]/div[{counter_linha}]/div[4]').text
+      stage14 = driver.find_element_by_xpath(f'/html/body/div[1]/div/div/div/div[2]/div/div[4]/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div[2]/div[{counter_linha}]/div[5]').text
+      stage33 = driver.find_element_by_xpath(f'/html/body/div[1]/div/div/div/div[2]/div/div[4]/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div[2]/div[{counter_linha}]/div[6]').text
+      stage46 = driver.find_element_by_xpath(f'/html/body/div[1]/div/div/div/div[2]/div/div[4]/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div[2]/div[{counter_linha}]/div[7]').text 
+      data = (nome,tier, pickrate, place, top4, win, stage14, stage33, stage46)
+      insert_data(data)
       counter_linha += 1  
       counter_linha_nome += 1
   else:
-    return data
+    print('Dados inseridos na tabela')
     
     ## /html/body/div[1]/div/div/div/div[2]/div/div[4]/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div[2]/div[2]/div[1]/div
     ## /html/body/div[1]/div/div/div/div[2]/div/div[4]/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div[2]/div[2]/div[2]
@@ -79,8 +91,9 @@ def get_data():
     ## /html/body/div[1]/div/div/div/div[2]/div/div[4]/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div[2]/div[1]/div[3]
 def main():
       adding_filters(tier='gold')
-      data = get_data()
-      print(data)
-    
-      
+      get_data(tier='gold')
+      print('FIM')
+     
+     
+         
 main()      
