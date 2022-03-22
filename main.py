69a91a98b1846b3e9f464f707b2a187a409c861b
wenked <wenked.matheus@yahoo.com.br>
2022-03-22 17:15:52 -0300

    scrapping data

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from webdriver_manager.firefox import GeckoDriverManager
+from selenium.common import exceptions
 import time
 
 driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
@@ -8,7 +9,7 @@ driver.get('https://tactics.tools/augments')
 
 
 def adding_filters(tier='silver'):
-    time.sleep(3)
+    time.sleep(5)
     # alerta cookies
     print('estou aqui')
     driver.find_element_by_xpath('/html/body/div[2]/div/div/div[3]/div[1]/button[2]').click()
@@ -38,12 +39,48 @@ def adding_filters(tier='silver'):
     driver.find_element_by_xpath('/html/body/div[3]/div[3]/ul/li[2]').click()
 
 
+## nome2 --   /html/body/div[1]/div/div/div/div[2]/div/div[4]/div[2]/div/div[2]/div/div/div[2]/div/div[1]/div[3]/div/div/div[2]
 
+## nome --    /html/body/div[1]/div/div/div/div[2]/div/div[4]/div[2]/div/div[2]/div/div/div[2]/div/div[1]/div[2]/div/div/div[2]
+## pickrate --/html/body/div[1]/div/div/div/div[2]/div/div[4]/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div[2]/div[1]/div[1]/div          
+## place --   /html/body/div[1]/div/div/div/div[2]/div/div[4]/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div[2]/div[1]/div[2]
+## top4 --    /html/body/div[1]/div/div/div/div[2]/div/div[4]/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div[2]/div[1]/div[3]
+
+def get_data():
+  counter_linha_nome = 2
+  counter_linha = 1
+  data = []
+  nome =  driver.find_element_by_xpath(f'/html/body/div[1]/div/div/div/div[2]/div/div[4]/div[2]/div/div[2]/div/div/div[2]/div/div[1]/div[{counter_linha_nome}]/div/div/div[2]')
+  while nome:
+      augment = {}
+      try:
+        nome =  driver.find_element_by_xpath(f'/html/body/div[1]/div/div/div/div[2]/div/div[4]/div[2]/div/div[2]/div/div/div[2]/div/div[1]/div[{counter_linha_nome}]/div/div/div[2]')
+        augment['nome'] = nome.text
+      except exceptions.NoSuchElementException:
+        nome = False
+        continue     
+      pickrate = driver.find_element_by_xpath(f'/html/body/div[1]/div/div/div/div[2]/div/div[4]/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div[2]/div[{counter_linha}]/div[1]/div')
+      place = driver.find_element_by_xpath(f'/html/body/div[1]/div/div/div/div[2]/div/div[4]/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div[2]/div[{counter_linha}]/div[2]')
+      top4 = pickrate = driver.find_element_by_xpath(f'/html/body/div[1]/div/div/div/div[2]/div/div[4]/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div[2]/div[{counter_linha}]/div[3]')
+      augment['pickrate'] = pickrate.text
+      augment['place'] = place.text
+      augment['top4'] = top4.text
+      print(augment)
+      data.append(augment)
+      counter_linha += 1  
+      counter_linha_nome += 1
+  else:
+    return data
+    
+    ## /html/body/div[1]/div/div/div/div[2]/div/div[4]/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div[2]/div[2]/div[1]/div
+    ## /html/body/div[1]/div/div/div/div[2]/div/div[4]/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div[2]/div[2]/div[2]
+    ## /html/body/div[1]/div/div/div/div[2]/div/div[4]/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div[2]/div[2]/div[3]
+    
+    ## /html/body/div[1]/div/div/div/div[2]/div/div[4]/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div[2]/div[1]/div[3]
 def main():
-    try:
-      adding_filters(tier='prismatic')
-    except Exception as e:
-      print(f'Error: {e}')
-      driver.close()  
+      adding_filters(tier='gold')
+      data = get_data()
+      print(data)
+    
       
 main()      
